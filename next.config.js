0838784f3eea07b23c499a1a6246470ad9d8ecfa
nir-segev <nir.segev@coinmama.com>
2024-03-10 15:59:25 +0200

    nextConfig images domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+    domains: ['awshelloworldbucket2.s3.amazonaws.com'],
+  },
+}
 
 module.exports = nextConfig
